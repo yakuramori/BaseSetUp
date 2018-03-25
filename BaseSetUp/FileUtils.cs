4131ba21a8e9dbb11eec1ee8fd80a7d1a162798e
Yury <yury.dubinin@gmail.com>
2018-03-25 15:52:05 +0200

    Added FileUtils with means to check file existence.

---
+++ BaseSetUp/FileUtils.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using log4net;
+
+namespace BaseSetUp
+{
+    public static class FileUtils
+    {
+        private static readonly ILog Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        private const string GenericDownloadFolder = @"C:\Users\%%\Downloads";
+
+        public static bool IfFileExistsInFolder(string fileName, string folder)
+        {
+            if (string.IsNullOrEmpty(fileName) || string.IsNullOrEmpty(folder))
+            {
+                throw new ArgumentException($"Invalid file name or folder. File name: {fileName} Folder: {folder}");
+            }
+
+            var path = $@"{folder}\{fileName}";
+            var result = File.Exists(path);
+            Log.Info($"File: {path} exists = {result}");
+            return result;
+        }
+
+        public static bool IfFileExistsInDownloadFolder(string fileName)
+        {
+            if (string.IsNullOrEmpty(fileName)) throw new ArgumentException($"Invalif file name: {fileName}");
+            return IfFileExistsInFolder(fileName, GetDownloadFolder());
+        }
+
+        private static string GetDownloadFolder()
+        {
+            var userName = System.Security.Principal.WindowsIdentity.GetCurrent().Name.Split('\\')[1];
+            var sysDownloadFolder = GenericDownloadFolder.Replace("%%", userName);
+            return sysDownloadFolder;
+        }
+    }
+}
